45946a47751b41fd072a707a6dd5532ba512aab2
Lalit Kumar Dudeja <info@lalitdudeja.com>
2021-10-16 20:07:12 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package WPStarter
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPStarter WordPress Theme Development123</title>
+</head>
+<body>
+    
+</body>
+</html>
