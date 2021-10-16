b3d4a808219bc52a646fe22f5c7ca2634f23ce96
Lalit Kumar Dudeja <info@lalitdudeja.com>
2021-10-16 21:02:33 +0530

    index.php modified once again

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WPStarter WordPress Theme Development</title>
+    <title>WPStarter WordPress Theme Development Demo</title>
 </head>
 <body>
     
